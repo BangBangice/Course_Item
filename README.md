94924b56e2544c0fe53cf6a3947da1baa593ba73
lee <343750282@qq.com>
2021-11-26 23:24:14 +0800

    10.29进度：实现了两个Activity之间的转移及传递数据

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 init
+10.16
+RecycleView的使用
+10.22
+RecyclerView添加删除数据及ContextMenu
+10.29	
+两个Activity之间的转移及传递数据
--- app/src/main/java/com/jnu/course_item/BookListMainActivity.java
+++ app/src/main/java/com/jnu/course_item/BookListMainActivity.java
@@ -1,12 +1,14 @@
 package com.jnu.course_item;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextMenu;
 import android.view.LayoutInflater;
@@ -24,7 +26,32 @@ import java.util.List;
 
 public class BookListMainActivity extends AppCompatActivity {
 
+    public static final int RESULT_CODE_ADD_DATA = 996;
+    public static final int REQUEST_CODE_ADD = 123;
+    public static final int REQUEST_CODE_EDIT = REQUEST_CODE_ADD+1;
     List<Book> books;
+    private MyRecyclerViewAdapter recyclerViewAdapter;
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode== REQUEST_CODE_ADD){     //请求的代码是123
+            if(resultCode== RESULT_CODE_ADD_DATA){    //返回的代码是996
+                String name=data.getStringExtra("name");
+                int position=data.getIntExtra("position",books.size()); //最后
+                books.add(position,new Book(name,R.drawable.book_3));
+                recyclerViewAdapter.notifyItemInserted(position);
+            }
+
+        }
+        if(requestCode== REQUEST_CODE_EDIT){     //请求的代码是123
+                String name=data.getStringExtra("name");
+                int position=data.getIntExtra("position",books.size()); //最后
+                books.get(position).setName(name);
+                recyclerViewAdapter.notifyItemChanged(position);
+            }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,7 +64,8 @@ public class BookListMainActivity extends AppCompatActivity {
         LinearLayoutManager layoutManager=new LinearLayoutManager(this);
         mainRecycleView.setLayoutManager(layoutManager);
 
-        mainRecycleView.setAdapter(new MyRecyclerViewAdapter(books));
+        recyclerViewAdapter = new MyRecyclerViewAdapter(books);
+        mainRecycleView.setAdapter(recyclerViewAdapter);
     }
     public List<Book> getListBooks(){
         return books;
@@ -49,12 +77,12 @@ public class BookListMainActivity extends AppCompatActivity {
         books.add(new Book("信息安全数学基础(第2版)",R.drawable.book_3));
     }
 
-    private class MyRecyclerViewAdapter extends RecyclerView.Adapter {
+    private class MyRecyclerViewAdapter extends RecyclerView.Adapter {   //为recyclerview写适配器
         private final List<Book> books;
 
         public MyRecyclerViewAdapter(List<Book> books) {
             this.books = books;
-        }
+        } //将外部数据赋值给内部
         @NonNull
         @Override
         public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -76,7 +104,10 @@ public class BookListMainActivity extends AppCompatActivity {
         }
 
 
-        private class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {  //联系布局和内存的，资源绑定器
+        private class MyViewHolder extends RecyclerView.ViewHolder implements View.OnCreateContextMenuListener, MenuItem.OnMenuItemClickListener {
+            public static final int CONTEX_MENU_ID_ADD = 1;
+            public static final int CONTEX_MENU_ID_UPDATE = 2;
+            public static final int CONTEX_MENU_ID_DELETE = 3;
             private ImageView imageView;
             private TextView textViewName;
 
@@ -101,37 +132,44 @@ public class BookListMainActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem menuItem) {
                 int position= getAdapterPosition();
+                Intent intent;
                 switch(menuItem.getItemId())
                 {
-                    case 1:
-                        View dialagueView= LayoutInflater.from(BookListMainActivity.this).inflate(R.layout.dialogue_input_item,null);
-                        AlertDialog.Builder alertDialogBuiler = new AlertDialog.Builder(BookListMainActivity.this);
-                        alertDialogBuiler.setView(dialagueView);
-
-                        alertDialogBuiler.setPositiveButton("确定",new DialogInterface.OnClickListener(){
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-                                EditText editName=dialagueView.findViewById(R.id.edit_text_name);
-                                //EditText editPrice=dialagueView.findViewById(R.id.edit_text_price);
-                                books.add(position,new Book(editName.getText().toString(),R.drawable.book_3));
-                                MyRecyclerViewAdapter.this.notifyItemInserted(position);
-                            }
-                        });
-                        alertDialogBuiler.setCancelable(false).setNegativeButton ("取消",new DialogInterface.OnClickListener(){
-                            @Override
-                            public void onClick(DialogInterface dialogInterface, int i) {
-
-                            }
-                        });
-                        alertDialogBuiler.create().show();;
+                    case CONTEX_MENU_ID_ADD:
+//                        View dialagueView= LayoutInflater.from(BookListMainActivity.this).inflate(R.layout.dialogue_input_item,null);
+//                        AlertDialog.Builder alertDialogBuiler = new AlertDialog.Builder(BookListMainActivity.this);
+//                        alertDialogBuiler.setView(dialagueView);
+//
+//                        alertDialogBuiler.setPositiveButton("确定",new DialogInterface.OnClickListener(){
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//                                EditText editName=dialagueView.findViewById(R.id.edit_text_name);
+//                                //EditText editPrice=dialagueView.findViewById(R.id.edit_text_price);
+//                                books.add(position,new Book(editName.getText().toString(),R.drawable.book_3));
+//                                MyRecyclerViewAdapter.this.notifyItemInserted(position);
+//                            }
+//                        });
+//                        alertDialogBuiler.setCancelable(false).setNegativeButton ("取消",new DialogInterface.OnClickListener(){
+//                            @Override
+//                            public void onClick(DialogInterface dialogInterface, int i) {
+//
+//                            }
+//                        });
+//                        alertDialogBuiler.create().show();;
+                        //对话框，不要了
+                        intent=new Intent(BookListMainActivity.this,InputActivity.class);
+                        intent.putExtra("position",position);
+                        BookListMainActivity.this.startActivityForResult(intent, REQUEST_CODE_ADD);
 
                         break;
-                    case 2:
-                        books.get(position).setName("测试修改");
-                        MyRecyclerViewAdapter.this.notifyItemChanged(position);
+                    case CONTEX_MENU_ID_UPDATE:
+                        intent=new Intent(BookListMainActivity.this,InputActivity.class);
+                        intent.putExtra("position",position);
+                        intent.putExtra("name",books.get(position).getTitle());
+                        BookListMainActivity.this.startActivityForResult(intent, REQUEST_CODE_EDIT);
                         break;
 
-                    case 3:
+                    case CONTEX_MENU_ID_DELETE:
                         books.remove(position);
                         MyRecyclerViewAdapter.this.notifyItemRemoved(position);
                         break;
@@ -143,9 +181,9 @@ public class BookListMainActivity extends AppCompatActivity {
             @Override
             public void onCreateContextMenu(ContextMenu contextMenu, View view, ContextMenu.ContextMenuInfo contextMenuInfo) {
                 int position=getAdapterPosition();
-                MenuItem menuItemAdd=contextMenu.add(Menu.NONE,1,1,"Add"+position);
-                MenuItem menuItemEdit=contextMenu.add(Menu.NONE,2,2,"Edit"+position);
-                MenuItem menuItemelete=contextMenu.add(Menu.NONE,3,3,"Delete"+position);
+                MenuItem menuItemAdd=contextMenu.add(Menu.NONE, CONTEX_MENU_ID_ADD, CONTEX_MENU_ID_ADD,"Add"+position);
+                MenuItem menuItemEdit=contextMenu.add(Menu.NONE, CONTEX_MENU_ID_UPDATE, CONTEX_MENU_ID_UPDATE,"Edit"+position);
+                MenuItem menuItemelete=contextMenu.add(Menu.NONE, CONTEX_MENU_ID_DELETE, CONTEX_MENU_ID_DELETE,"Delete"+position);
 
                 menuItemAdd.setOnMenuItemClickListener(this);
                 menuItemEdit.setOnMenuItemClickListener(this);
