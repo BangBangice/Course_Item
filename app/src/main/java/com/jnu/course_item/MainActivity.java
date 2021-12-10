9d5a1fd02c737c61fbe92cf085637e9455bcf4ad
lee <343750282@qq.com>
2021-12-10 23:39:27 +0800

    11.12进度：将booklistMainActivity的功能转移到了fragment中。用tablayout实现切换页面下方的Vieepage2.

---
+++ app/src/main/java/com/jnu/course_item/MainActivity.java
@@ -0,0 +1,60 @@
+package com.jnu.course_item;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewPager2 viewPagerFragments=findViewById(R.id.viewpager2_content);
+        viewPagerFragments.setAdapter(new MyFragmentAdpater(this));
+
+        TabLayout tabLayoutHeader= findViewById(R.id.tablayout_header);
+        TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(tabLayoutHeader, viewPagerFragments, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                tab.setText("biaoti"+position);
+            }
+        });
+        tabLayoutMediator.attach();
+
+    }
+
+    private class MyFragmentAdpater extends FragmentStateAdapter {
+
+
+        public MyFragmentAdpater(@NonNull FragmentActivity fragmentActivity) {
+            super(fragmentActivity);
+        }
+
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            switch(position)
+            {
+                case 0:
+                    return BookListFragment.newInstance();
+                default:
+                    return WebViewFragment.newInstance();
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return 3;
+        }
+    }
+}
